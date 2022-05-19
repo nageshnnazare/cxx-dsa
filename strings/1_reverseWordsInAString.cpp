ab6084a79fc0185aa31ac9ad2fd19fe3c0742c9e
nageshnnazare <nageshnnazare@gmail.com>
2022-05-19 23:50:28 +0530

    added string problems

---
+++ strings/1_reverseWordsInAString.cpp
@@ -0,0 +1,39 @@
+#include <iostream>
+#include <string>
+
+std::string reverseWordsInString(std::string s) {
+    int left = 0;
+    int right = s.length() - 1;
+
+    std::string temp = "";
+    std::string ans = "";
+
+    while (left <= right) {
+        char ch = s[left];
+        if (ch != ' ') {
+            temp += ch;
+        }
+        else if (ch == ' ') {
+            if (ans != "") ans = temp + " " + ans;
+            else ans = temp;
+            temp = "";
+        }
+        left++;
+    }
+
+    if (temp != "") {
+        if (ans != "") ans = temp + " " + ans;
+        else ans = temp;
+    }
+
+    return ans;
+}
+
+int main(int argc, char const* argv[]) {
+    std::string st = "this is a code for reversing a string";
+    std::cout << "Before reversing words: " << std::endl;
+    std::cout << st << std::endl;
+    std::cout << "After reversing words: " << std::endl;
+    std::cout << reverseWordsInString(st) << std::endl;
+    return 0;
+}
